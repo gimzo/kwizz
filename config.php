8a46d82f73e489f51e697215148939c024e28a92
David <gimzo.0@gmail.com>
2013-05-06 20:57:02 +0200

    prvi commit, pocetni config file napravljen

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/****************************
+ *    Konfiguracija
+ *
+ */
+
+// MySQL podaci:
+$db_server='localhost';
+$db_baza='kwizz';
+$db_username='kwizz';
+$db_password='password1234';
+
+
+
+/****************
+Kraj konfiguracije
+Dragons below this line:
+**********************/
+
+// Ovdje dodajemo globalne funkcije kad nam budu trebale.
+?>
