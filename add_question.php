cbff3453086b71840d79a83c4cf07fd033c8b714
Paolo Perkovic <paoloperkovic@Paolos-MacBook-Pro.local>
2013-05-09 22:59:03 +0200

    Sesija povezana sa add_question.php
    Iz sesije se vuce podatak o id-u korisnika koji je trenutno ulogiran i
    postavlja se kao autor pitanja. Izmjenjene neke male stvari.

--- add_question.php
+++ add_question.php
@@ -1,10 +1,14 @@
+<?php
+  session_start();
+?>
+
 <!DOCTYPE html> 
 <html>
 <head>
   <title>Add question</title>
 </head>
 <body>
-	<form action="add_question.php" method="POST">
+  <form action="add_question.php" method="POST">
 		<p>Pitanje: <input type="text" name="tekst_pitanja" maxlength="100"></p>
     <p>a: <input type="text" name="odgovor_a" maxlegth="45">
     <input type="checkbox" name="tocan_a"></p>
@@ -23,99 +27,83 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') 
 {
 	$tekst=$_POST['tekst_pitanja'];
-  $bodovi=$_POST['bodovi'];
-  $vrsta=1; //visestruki izbor
-  $jezik="hr";
-  $autor=2; //id_autor izvuci iz sesije
-  $checked=0;
   $odgovor_a=$_POST['odgovor_a'];
   $odgovor_b=$_POST['odgovor_b'];
   $odgovor_c=$_POST['odgovor_c'];
   $odgovor_d=$_POST['odgovor_d'];
-  $tocan=1;
-  
+  $bodovi=$_POST['bodovi'];
+  $vrsta=1; //visestruki izbor
+  $jezik="hr";
 
 	include 'config.php';
 	db_connect();
 	
-  //provjera da li su svi textboxi puni
-  if(empty($_POST['tekst_pitanja']) || empty($_POST['odgovor_a']) || empty($_POST['odgovor_b']) || empty($_POST['odgovor_c']) || empty($_POST['odgovor_d']))
-  {
-    print '<script type="text/javascript">';
-    print 'alert("niste popunili sve textboxe")';
-    print '</script>';  
-  }
+  if (isset($_SESSION['user'])) {
+    
+    $user=$_SESSION['user'];
+    $result=mysqli_query($mysqli, "SELECT id_korisnik FROM korisnik WHERE nadimak_korisnik = '$user';");
+    $data=mysqli_fetch_array($result);
+    $autor=$data['id_korisnik'];
 
-  else
-  {
-  //provjera da li su checkboxi checkirani, upisivanje pitanja i odg u bazu
-    if(empty ($_POST['tocan_a']) && empty($_POST['tocan_b']) && empty($_POST['tocan_c']) && empty($_POST['tocan_d']))
+    //provjera da li su svi textboxi puni
+    if(empty($_POST['tekst_pitanja']) || empty($_POST['odgovor_a']) || empty($_POST['odgovor_b']) || empty($_POST['odgovor_c']) || empty($_POST['odgovor_d']))
     {
       print '<script type="text/javascript">';
-      print 'alert("niste odabrali nijedan odgovor")';
+      print 'alert("niste popunili sve textboxe")';
       print '</script>';  
     }
-
     else
     {
-      mysqli_query($mysqli, "INSERT INTO `pitanje` (`tekst_pitanja`, `vrsta_pitanja`, `bodovi_pitanja`, `jezik_pitanja`, `id_autor`) VALUES ('$tekst', '$vrsta', '$bodovi', '$jezik', '$autor');");
-      $id=mysqli_insert_id($mysqli);
-
-      if (empty($_POST['tocan_a']))
+      //provjera da li su checkboxi checkirani, upisivanje pitanja i odg u bazu
+      if(empty ($_POST['tocan_a']) && empty($_POST['tocan_b']) && empty($_POST['tocan_c']) && empty($_POST['tocan_d']))
       {
-        $tocan=0;
-        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_a', '$tocan', '$id');");    
+        print '<script type="text/javascript">';
+        print 'alert("niste odabrali nijedan odgovor")';
+        print '</script>';  
       }
-
-      else
+     else
       {
+        mysqli_query($mysqli, "INSERT INTO `pitanje` (`tekst_pitanja`, `vrsta_pitanja`, `bodovi_pitanja`, `jezik_pitanja`, `id_autor`) VALUES ('$tekst', '$vrsta', '$bodovi', '$jezik', '$autor');");
+        $id=mysqli_insert_id($mysqli);
+
         $tocan=1;
+        if (empty($_POST['tocan_a']))
+        {
+          $tocan=0;   
+        }
         mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_a', '$tocan', '$id');"); 
-      }
-
-      if(empty($_POST['tocan_b']))
-      {
-        $tocan=0;
-        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_b', '$tocan', '$id');");    
-      }
-
-      else
-      {
+      
         $tocan=1;
-        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_b', '$tocan', '$id');"); 
-      }
+        if(empty($_POST['tocan_b']))
+        {
+          $tocan=0;    
+        }
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_b', '$tocan', '$id');");
 
-      if(empty($_POST['tocan_c']))
-      {
-        $tocan=0;
-        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_c', '$tocan', '$id');");    
-      }
-
-      else
-      {
         $tocan=1;
-        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_c', '$tocan', '$id');"); 
-      }
+        if(empty($_POST['tocan_c']))
+        {
+          $tocan=0;    
+        }
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_c', '$tocan', '$id');");
 
-      if(empty($_POST['tocan_d']))
-      {
-        $tocan=0;
-        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_d', '$tocan', '$id');");       
-      }
-
-      else
-      {
         $tocan=1;
-        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_d', '$tocan', '$id');"); 
+        if(empty($_POST['tocan_d']))
+        {
+          $tocan=0;       
+        }
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_d', '$tocan', '$id');");
       }
-  
     }
+
+  }else{
+    print '<script type="text/javascript">';
+    print 'alert("You are not logged in!")';
+    print '</script>';
   }
 
 	db_disconnect();
 }
-
 ?>
-
 </body>
 </html>
