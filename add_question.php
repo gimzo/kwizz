5e0212aa581d14bd9c2eb93d4c57b20196e00303
Iva Petrovic <iva.petrovic5@gmail.com>
2013-05-09 22:16:36 +0200

    dodavanje pitanja i odgovora u bazu
    Prva verzija add_question.php-osposobljen je upis pitanja i odgovora u
    bazu, napravljena provjera da li su svi textboxi ispunjeni i da li je
    barem jedan checbox chekiran. Vrijednost vrste pitanja postavljena na
    1(visestruki izbor),potrebno je jos doraditi i osposobiti i druge vrste
    pitanja, korisnika sam postavila na 2, kad se sredi sesija rijesit ce se
    i to i jezik

---
+++ add_question.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html> 
+<html>
+<html>
+<head>
+   
+</head>
+<body>
+	<form action="add_question.php" method="POST">
+		<p>Pitanje: <input type="text" id="tekst_pitanja" name="tekst_pitanja" maxlength="100"></p>
+    <p>a: <input type="text" id="odgovor_a" name="odgovor_a" maxlegth="45">
+    <input type="checkbox" name="tocan_a"></p>
+    <p>b: <input type="text" id="odgovor_b" name="odgovor_b" maxlegth="45">
+    <input type="checkbox" name="tocan_b"></p>
+    <p>c: <input type="text" id="odgovor_c" name="odgovor_c" maxlegth="45">
+    <input type="checkbox" name="tocan_c"></p>
+    <p>d: <input type="text" id="odgovor_d" name="odgovor_d" maxlegth="45">
+    <input type="checkbox" name="tocan_d"></p>
+    <p><input type="text" name="bodovi"></p>
+    <p><input type="submit" value="potvrdi" ></p>
+  </form> 
+
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{
+	$tekst=$_POST['tekst_pitanja'];
+  $bodovi=$_POST['bodovi'];
+  $vrsta=1; //visestruki izbor
+  $jezik="hr";
+  $autor=2; //id_autor izvuci iz sesije
+  $checked=0;
+  $odgovor_a=$_POST['odgovor_a'];
+  $odgovor_b=$_POST['odgovor_b'];
+  $odgovor_c=$_POST['odgovor_c'];
+  $odgovor_d=$_POST['odgovor_d'];
+  $tocan=1;
+  
+
+	include 'config.php';
+	db_connect();
+	
+  //provjera da li su svi textboxi puni
+  if(empty($_POST['tekst_pitanja']) || empty($_POST['odgovor_a']) || empty($_POST['odgovor_b']) || empty($_POST['odgovor_c']) || empty($_POST['odgovor_d']))
+  {
+    print '<script type="text/javascript">';
+    print 'alert("niste popunili sve textboxe")';
+    print '</script>';  
+  }
+
+  else
+  {
+  //provjera da li su checkboxi checkirani, upisivanje pitanja i odg u bazu
+    if(empty ($_POST['tocan_a']) && empty($_POST['tocan_b']) && empty($_POST['tocan_c']) && empty($_POST['tocan_d']))
+    {
+      print '<script type="text/javascript">';
+      print 'alert("niste odabrali nijedan odgovor")';
+      print '</script>';  
+    }
+
+    else
+    {
+      mysqli_query($mysqli, "INSERT INTO `pitanje` (`tekst_pitanja`, `vrsta_pitanja`, `bodovi_pitanja`, `jezik_pitanja`, `id_autor`) VALUES ('$tekst', '$vrsta', '$bodovi', '$jezik', '$autor');");
+      $id=mysqli_insert_id($mysqli);
+
+      if (empty($_POST['tocan_a']))
+      {
+        $tocan=0;
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_a', '$tocan', '$id');");    
+      }
+
+      else
+      {
+        $tocan=1;
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_a', '$tocan', '$id');"); 
+      }
+
+      if(empty($_POST['tocan_b']))
+      {
+        $tocan=0;
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_b', '$tocan', '$id');");    
+      }
+
+      else
+      {
+        $tocan=1;
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_b', '$tocan', '$id');"); 
+      }
+
+      if(empty($_POST['tocan_c']))
+      {
+        $tocan=0;
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_c', '$tocan', '$id');");    
+      }
+
+      else
+      {
+        $tocan=1;
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_c', '$tocan', '$id');"); 
+      }
+
+      if(empty($_POST['tocan_d']))
+      {
+        $tocan=0;
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_d', '$tocan', '$id');");       
+      }
+
+      else
+      {
+        $tocan=1;
+        mysqli_query($mysqli, "INSERT INTO `odgovor` (`tekst_odgovor`, `tocan_odgovor`, `id_pitanje`) VALUES ('$odgovor_d', '$tocan', '$id');"); 
+      }
+  
+    }
+  }
+
+	db_disconnect();
+}
+
+?>
+
+</body>
+</html>
