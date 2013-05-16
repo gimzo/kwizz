2c17573a5f50a4e125bb9ec0ff9ede99bca39bf5
Paolo Perkovic <paoloperkovic@Paolos-MacBook-Pro.local>
2013-05-17 00:27:56 +0200

    Sredjen dizajn registracije, osigruan login i registracija
    Sigurniji login i register, gotov dizajn registracije

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
 		if (!isset($_SESSION['user'])) {
 			include_once 'config.php';
 
-			// TO-DO: Sigurnost logina
-
 			$nickname=$_POST['nickname'];
 			$password=$_POST['password'];
 
 			db_connect();
+
+			$nickname=mysqli_real_escape_string($mysqli, $nickname);
+			$password=mysqli_real_escape_string($mysqli, $password);
 
 			$result=mysqli_query($mysqli, "SELECT id_korisnik FROM korisnik WHERE nadimak_korisnik = '$nickname' AND password_korisnik = md5('$password');");
 			if (mysqli_num_rows($result)==1) {
