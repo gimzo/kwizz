21cd12704cfeeb5f82a104b370996712f30d4f69
bcr3ative <paolo.perkovic@gmail.com>
2014-01-25 11:49:05 +0100

    Fix za logout
    - Ispravljena greska kod logouta posto se nije iz sesije brisao id korisnika
    - Sad se brisu sve varijable i bilo sto sto se naknadno doda u sesiju

--- logout.php
+++ logout.php
@@ -1,8 +1,16 @@
 <?php
 	session_start();
 
+	// Ako je korisnik ulogiran izvrsava sign out
 	if (isset($_SESSION['user'])) {
-		unset($_SESSION['user']);
+		// Oslobada sve varijable sesije
+		session_unset();
+		// Unistava sve podatka registrirane u sesiji
+		session_destroy();
+		// Pridruzuje prazno polje sesiji
+		$_SESSION = array();
 	}
+	
+	// Preusmjeravanje na index.php nakon sign out-a ili posjeta neulogiranom korisniku
 	header('Location: index.php');
 ?>
