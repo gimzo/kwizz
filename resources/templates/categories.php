bdd01fa3259ab1a842c5a9e714980b77e81284b4
bcr3ative <paolo.perkovic@gmail.com>
2014-01-30 01:15:38 +0100

    Dodan count pitanja categories.php
    - Dodan count pitanja za svaku kategoriju u categories.php

--- resources/templates/categories.php
+++ resources/templates/categories.php
@@ -4,13 +4,15 @@
 		<div class="row">
 			<?php
 			db_connect();
-							// Nadkategorije
+			// Nadkategorije
 			$stmt = $mysqli->prepare("SELECT id_kategorija, naziv_kategorija FROM kategorija WHERE nadkategorija IS NULL");
 			$stmt->execute();
 			$res = $stmt->get_result();
 
-							// Podkategorije
+			// Podkategorije
 			$stmt2 = $mysqli->prepare("SELECT id_kategorija, naziv_kategorija FROM kategorija WHERE nadkategorija=? LIMIT 5");
+			// Broj pitanja u kategoriji
+			$stmt3 = $mysqli->prepare("SELECT COUNT(*) FROM pitanje_kategorija WHERE id_kategorija=?");
 
 			while ($category = $res->fetch_array()) {
 				echo '
@@ -21,8 +23,12 @@
 						$stmt2->execute();
 						$res2 = $stmt2->get_result();
 						while ($subcategory = $res2->fetch_array()) {
+							$stmt3->bind_param('i', $subcategory['id_kategorija']);
+							$stmt3->execute();
+							$res3 = $stmt3->get_result();
+							$qcount = $res3->fetch_array();
 							echo '
-							<a href="play.php?category='.$subcategory['id_kategorija'].'" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp;&nbsp;'.$subcategory['naziv_kategorija'].'</a>
+							<a href="play.php?category='.$subcategory['id_kategorija'].'" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp;&nbsp;'.$subcategory['naziv_kategorija'].($qcount[0] > 0 ? '<span class="badge">'.$qcount[0].'</span>' : '').'</a>
 							';
 						}
 						echo '
@@ -31,6 +37,7 @@
 				';
 			}
 			
+			$stmt3->close();
 			$stmt2->close();
 			$stmt->close();
 			db_disconnect();
